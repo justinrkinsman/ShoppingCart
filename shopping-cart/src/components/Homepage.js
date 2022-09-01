5ab5cb61ff7b3a5c077d9a4e856ef239d551b819
Justin <justinrkinsman@gmail.com>
2022-09-01 14:20:01 -0300

    Complete basic layout for bestsellers components on homepage

--- shopping-cart/src/components/Homepage.js
+++ shopping-cart/src/components/Homepage.js
@@ -1,12 +1,14 @@
 import './Homepage.css';
 import { Navbar } from './Navbar'
 import { About } from './homePageComponents/About'
+import { Bestsellers } from './homePageComponents/Bestsellers'
 
 export function Homepage() {
   return (
     <div className="Homepage">
       <Navbar />
       <About />
+      <Bestsellers />
     </div>
   );
 }
---
+++ shopping-cart/src/components/homePageComponents/Bestsellers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//{/*import Cards from './shopComponents/Cards' */}
+
+export function Bestsellers() {
+    return(
+        <div className="bestsellers">
+            <p>Bestsellers</p>
+            {/*<Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />*/}
+        </div>
+    )
+}
