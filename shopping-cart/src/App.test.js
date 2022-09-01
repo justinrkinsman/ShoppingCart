5cce329e1e038b1a233f2f86e47ff7bafc235e12
Justin <justinrkinsman@gmail.com>
2022-09-01 10:56:04 -0300

    Create components folder

--- shopping-cart/src/App.test.js
+++ shopping-cart/src/App.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import { Homepage } from './Homepage';
+import { Homepage } from './components/Homepage';
 import React from "react"
 import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"
 
-test('renders learn react link', () => {
+test('render company name to homepage', () => {
   render(<Homepage />);
   const linkElement = screen.getByText('Bloodbath Records');
   expect(linkElement).toBeInTheDocument();
