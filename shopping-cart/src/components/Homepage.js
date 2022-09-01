9cdf256f3a75afaa224a4fc383acc944968b7514
Justin <justinrkinsman@gmail.com>
2022-09-01 13:59:40 -0300

    Achieve basic layout for about section

--- shopping-cart/src/components/Homepage.js
+++ shopping-cart/src/components/Homepage.js
@@ -1,10 +1,12 @@
 import './Homepage.css';
 import { Navbar } from './Navbar'
+import { About } from './homePageComponents/About'
 
 export function Homepage() {
   return (
     <div className="Homepage">
       <Navbar />
+      <About />
     </div>
   );
 }
---
+++ shopping-cart/src/components/homePageComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css'
+
+export function About() {
+    return (
+        <div className="aboutSection">
+            <h1>Welcome to the BLOODBATH</h1>
+            <p>Bloodbath Records is a fictional record label founded in September 2022 by Justin Kinsman for 
+                a shopping cart project he made on <a href='https://www.theodinproject.com/dashboard'><em>
+                    The Odin Project</em></a>. Buying these records is impossible.
+                No money will be moved. You will receive nothing. You have been warned!
+            </p>
+        </div>
+    )
+}
