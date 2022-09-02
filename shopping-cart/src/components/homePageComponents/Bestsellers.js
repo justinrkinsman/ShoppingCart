56dca716792e62869d9057df4a246010990544f8
Justin <justinrkinsman@gmail.com>
2022-09-02 10:43:04 -0300

    Import albums cover to bestsellers page

--- shopping-cart/src/components/homePageComponents/Bestsellers.js
+++ shopping-cart/src/components/homePageComponents/Bestsellers.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Card } from '../shopPageComponents/Card'
+import TSOP from '../../albums/TSOP.jpg'
+import Ride from '../../albums/RideTheLightning.jpg'
+import Among from '../../albums/AmongTheLiving.jpg'
+import Alice from '../../albums/AliceInHell.jpg'
+import Torture from '../../albums/Torture.jpg'
 
 export function Bestsellers() {
     return(
         <div className="bestsellers">
             <p>Bestsellers</p>
-            <Card source='' title='Ride the Lightning' band='Metallica'/>
-            <Card source='' title='The Sound of Perseverance' band='Death'/>
-            <Card source='' title='Among the Living' band='Anthrax'/>
-            <Card source='' title='Alice in Hell' band='Annihilator'/>
-            <Card source='' title='Torture' band='Cannibal Corpse'/>
+            <Card source={Ride} title='Ride the Lightning' band='Metallica'/>
+            <Card source={TSOP} title='The Sound of Perseverance' band='Death'/>
+            <Card source={Among} title='Among the Living' band='Anthrax'/>
+            <Card source={Alice} title='Alice in Hell' band='Annihilator'/>
+            <Card source={Torture} title='Torture' band='Cannibal Corpse'/>
         </div>
     )
 }
