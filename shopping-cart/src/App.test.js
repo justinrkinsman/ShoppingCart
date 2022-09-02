dbd5ba055eb225d0212bfc79ef7ec7092999e1e6
Justin <justinrkinsman@gmail.com>
2022-09-02 10:49:05 -0300

    Fix typos

--- shopping-cart/src/App.test.js
+++ shopping-cart/src/App.test.js
@@ -1,4 +1,4 @@
-import { getByText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from "react"
 import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"
@@ -6,14 +6,14 @@ import { Sidebar } from "./components/shopPageComponents/Sidebar"
 
 describe("Navbar Links", () => {
   it('Renders Homepage', () => {
-    const { getByText } = render(<a href="https://test.com">Home</a>);
+    render(<a href="https://test.com">Home</a>);
     const link = screen.getByText('Home')
     userEvent.click(link)
     expect(screen.getByText("Home")).toHaveAttribute('href', 'https://test.com')
   })
 
   it('Renders Shop Page', () => {
-    const { getByText } = render(<a href="http://test.com/shop">Shop</a>)
+    render(<a href="http://test.com/shop">Shop</a>)
     const link = screen.getByText('Shop')
     userEvent.click(link)
     expect(screen.getByText("Shop")).toHaveAttribute('href', 'http://test.com/shop')
--- shopping-cart/src/components/shopPageComponents/Sidebar.js
+++ shopping-cart/src/components/shopPageComponents/Sidebar.js
@@ -4,7 +4,7 @@ export const Sidebar = () => {
     return (
         <>
             <div className='cart'>
-                <img src='' alt='Image of cart'></img>
+                <img src='' alt='Display of cart'></img>
                 <p>Placeholder for cart quantity</p>
             </div>
         </>
