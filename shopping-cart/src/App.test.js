394673016e4355fef0465f9e0dcebf5191af1f9a
Justin <justinrkinsman@gmail.com>
2022-09-02 09:38:55 -0300

    PAss tests for routeswitch

--- shopping-cart/src/App.test.js
+++ shopping-cart/src/App.test.js
@@ -4,8 +4,18 @@ import React from "react"
 import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"
 
-test('render company name to homepage', () => {
-  render(<Homepage />);
-  const linkElement = screen.getByText('Bloodbath Records');
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Navbar Links", () => {
+  it('Renders Homepage', () => {
+    const { getByText } = render(<a href="https://test.com">Home</a>);
+    const link = getByText('Home')
+    userEvent.click(link)
+    expect(screen.getByText("Home").closest('a')).toHaveAttribute('href', 'https://test.com')
+  })
+
+  it('Renders Shop Page', () => {
+    const { getByText } = render(<a href="http://test.com/shop">Shop</a>)
+    const link = getByText('Shop')
+    userEvent.click(link)
+    expect(screen.getByText("Shop").closest('a')).toHaveAttribute('href', 'http://test.com/shop')
+  })
+})
