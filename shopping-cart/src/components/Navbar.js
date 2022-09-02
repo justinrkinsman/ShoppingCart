4f1386fbea9a3ff876be4ad830f304eff2a64fb6
Justin <justinrkinsman@gmail.com>
2022-09-02 09:19:55 -0300

    Add Links to navbar

--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 export function Navbar() {
     return (
     <div className='navbar'>
         <p>Bloodbath Records</p>
         <ul className='navbarLinks'>
-            <li>Home</li>
-            <li>Shop</li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/shop'>Shop</Link></li>
         </ul>
     </div>
     )
