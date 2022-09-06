20fda44218de2d9073d079a57dbe4012c5856a3a
Justin <justinrkinsman@gmail.com>
2022-09-06 09:08:51 -0300

    Add css file to bestsellers.js

--- shopping-cart/src/components/homePageComponents/Bestsellers.js
+++ shopping-cart/src/components/homePageComponents/Bestsellers.js
@@ -5,16 +5,19 @@ import Ride from '../../albums/RideTheLightning.jpg'
 import Among from '../../albums/AmongTheLiving.jpg'
 import Alice from '../../albums/AliceInHell.jpg'
 import Torture from '../../albums/Torture.jpg'
+import './Bestsellers.css'
 
 export function Bestsellers() {
     return(
         <div className="bestsellers">
             <p>Bestsellers</p>
-            <Card source={Ride} title='Ride the Lightning' band='Metallica' page='Go to Store'/>
-            <Card source={TSOP} title='The Sound of Perseverance' band='Death' page='Go to Store'/>
-            <Card source={Among} title='Among the Living' band='Anthrax' page='Go to Store'/>
-            <Card source={Alice} title='Alice in Hell' band='Annihilator' page='Go to Store'/>
-            <Card source={Torture} title='Torture' band='Cannibal Corpse' page='Go to Store'/>
+            <div className="albums">
+                <Card source={Ride} title='Ride the Lightning' band='Metallica' page='Go to Store'/>
+                <Card source={TSOP} title='The Sound of Perseverance' band='Death' page='Go to Store'/>
+                <Card source={Among} title='Among the Living' band='Anthrax' page='Go to Store'/>
+                <Card source={Alice} title='Alice in Hell' band='Annihilator' page='Go to Store'/>
+                <Card source={Torture} title='Torture' band='Cannibal Corpse' page='Go to Store'/>
+            </div>
         </div>
     )
 }
